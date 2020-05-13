24a534703a2c0b349bcd31ed93e41e3e851c0cda
sgotre <gotre@teraone.de>
2020-05-13 21:34:58 +0200

    Show phpinfo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # phpinfo-7
+
+## Display heroku's default php info for the latest PHP 7.x
---
+++ public/index.php
@@ -0,0 +1,9 @@
+
+<div style="text-align: center">
+
+    <h1>Heroku defaults for PHP 7.x</h1>
+    <h2>Source Code <a href="https://github.com/teraone/phpinfo-7">https://github.com/teraone/phpinfo-7</a></h2>
+
+</div>
+<?php
+phpinfo();
